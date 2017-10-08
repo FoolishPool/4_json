c7b870399364a792ada4e8e6fe732ed1a5ec401a
FoolishPool <32616345+FoolishPool@users.noreply.github.com>
2017-10-09 00:29:02 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,15 +1,15 @@
 import json
+import sys
 
 def load_data(filepath):
     with open(filepath, 'r') as file_json:
-        json_content = json.loads(file_json.read())
-        return json_content
+        file_content = json.loads(file_json.read())
+        return file_content
 
 def pretty_print_json(json_content):
     pretty_print = json.dumps(json_content, sort_keys=True, indent=4, ensure_ascii = False)
     print(pretty_print)
 
 if __name__ == '__main__':
-    path_to_file = input('Filepath: ')
-    json_content = load_data(path_to_file)
+    json_content = load_data(sys.argv[1])
     pretty_print_json(json_content)
